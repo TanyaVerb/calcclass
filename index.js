459da2eb82858afea1747eb3a12ebdcc58c9aa77
TanyaVerb <karra_t@mail.ru>
2024-04-11 23:39:46 +0300

    add function checkName

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const signSelect = document.querySelector(".sign");
 const resultTitle = document.querySelector(".result-value");
 const resultBtn = document.querySelector(".result-btn");
 const clearBtn = document.querySelector(".clear-btn");
-const checkName = document.querySelector(".checkname");
+const checkName = document.querySelector(".user-name");
 console.log(checkName);
 
 console.log(firstNumInput);
@@ -94,111 +94,98 @@ clearBtn.addEventListener("click", resetCalculate);
 // calculate.sum
 
 let obj = {
-  name: 'Tanya',
-  someFoo: function(param1, param2,param3){
-console.log (obj.someFoo.name); // 
-console.log (param1); // 
-console.log (param2); // 
-console.log (param3); // 
-console.log (obj.someFoo.arguments); // 
+  name: "Tanya",
+  someFoo: function (param1, param2, param3) {
+    console.log(obj.someFoo.name); //
+    console.log(param1); //
+    console.log(param2); //
+    console.log(param3); //
+    console.log(obj.someFoo.arguments); //
   },
   someFoo2: () => {
-    console.log (obj.someFoo2.name);
+    console.log(obj.someFoo2.name);
   },
-  someFoo3 ()  {
-    console.log (obj.someFoo3.name); // укороченная запись первой функции
-}
-}
-  
-obj.someFoo(1, 'ggg', false)
+  someFoo3() {
+    console.log(obj.someFoo3.name); // укороченная запись первой функции
+  },
+};
+
+obj.someFoo(1, "ggg", false);
 
 //
-const someFoo = function(){
-  console.log (someFoo.name); 
-}
-const someFooArrow = function(){
-  console.log (someFooArrow.name); 
-}
+const someFoo = function () {
+  console.log(someFoo.name);
+};
+const someFooArrow = function () {
+  console.log(someFooArrow.name);
+};
 
-
-function someFoo3(){
-  console.log (someFoo3.name);
+function someFoo3() {
+  console.log(someFoo3.name);
 }
-function sum1(num1,num2,num3,num4,num5,num6,num7 ){
-   
-  return num1+ num2 +num3 +num4 +num5 +num6 + num7 
+function sum1(num1, num2, num3, num4, num5, num6, num7) {
+  return num1 + num2 + num3 + num4 + num5 + num6 + num7;
 }
-function sum2 (){
-  console.log (sum2.arguments);
+function sum2() {
+  console.log(sum2.arguments);
   // return arguments[5]
-  const paramIndexLength = arguments.length -1 //8
-   let result = 0 //0
+  const paramIndexLength = arguments.length - 1; //8
+  let result = 0; //0
 
-  for (let i = 0; i<=paramIndexLength; i++ ){
-    const el = arguments[i] // arguments[1]
-    result += el // result = 
+  for (let i = 0; i <= paramIndexLength; i++) {
+    const el = arguments[i]; // arguments[1]
+    result += el; // result =
   }
-  return result
+  return result;
 }
 
 // let result1 = sum1(1,2,3,4,5,6,7 )
- let result2 = sum2(1,2,3,4,5,6,7,8,9)
-
-
-
- 
+let result2 = sum2(1, 2, 3, 4, 5, 6, 7, 8, 9);
 
 //  console.log (result1);
- console.log (result2);
+console.log(result2);
 
 //У функции ключ name и arguments
-//К ним мы можем обратиться через название функции и точку. При этом, к arguments может просто по слову 
+//К ним мы можем обратиться через название функции и точку. При этом, к arguments может просто по слову
 //Typeof функции возвращает 'function', на функции это объекты
 // у стрелочных функций нет key arguments
-
-
-
 
 // window.someFoo3()
 // someFoo ()
 // someFooArrow()
 // console.log (obj.name); //undefined
 
-const str = 'Hello';
-const arr = [ 'Hello', 'car', 'green']
-console.log (arr[1]);
-console.log (arr.toString());
-const myName = 'vlad'
-console.log (myName.length);
+const str = "Hello";
+const arr = ["Hello", "car", "green"];
+console.log(arr[1]);
+console.log(arr.toString());
+const myName = "vlad";
+console.log(myName.length);
 
-let index = 1
+let index = 1;
 
-console.log (myName[index]);
-console.log (myName.toUpperCase()); //метод возвращает новое значение не изменяя текущее
-console.log (myName);
-const upperCaseMyName = myName.toUpperCase()
+console.log(myName[index]);
+console.log(myName.toUpperCase()); //метод возвращает новое значение не изменяя текущее
+console.log(myName);
+const upperCaseMyName = myName.toUpperCase();
 
-function formatUserName(str){
-const lowerCase = str.toLowerCase().slice(1);
-const firstLetter = str[0].toUpperCase()
-console.log(lowerCase)
-console.log(firstLetter)
-return firstLetter +lowerCase
-
+function formatUserName(str) {
+  const lowerCase = str.toLowerCase().slice(1);
+  const firstLetter = str[0].toUpperCase();
+  console.log(lowerCase);
+  console.log(firstLetter);
+  return firstLetter + lowerCase;
 }
 
-
-
-
-console.log(formatUserName('VLAD'))
+console.log(formatUserName("VLAD"));
 
 // num1 - пользователи вводят свои имена в разных регистрах
 //на дополнительную кнопку (которую нужно создать)
 // в то место где показывался результат расчета - нужно выводить значение имени в формате того чтобы первый символ Анна, Света
 
-function checkUserName(userName) {
-  
+checkName.addEventListener("click", () => {
+  let data = firstNumInput.value;
+  const resultData = formatUserName(data);
 
-}
-
-checkName.addEventListener("click",checkUserName );
+  resultTitle.innerHTML = resultData;
+});
